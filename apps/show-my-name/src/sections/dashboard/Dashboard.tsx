7458a193794e2e793dd4309d35c0451df7be23f2
Francisco Marcano <fjmn2001@gmail.com>
2023-08-23 13:12:26 -0400

    adding dashboard component v2

--- apps/show-my-name/src/sections/dashboard/Dashboard.tsx
+++ apps/show-my-name/src/sections/dashboard/Dashboard.tsx
@@ -1,9 +1,23 @@
+interface CoWorker {
+  name: string;
+}
+
 export const Dashboard = () => {
+  const coWorkers: CoWorker[] = [{ name: "Name 1" }, { name: "Name 2" }];
+
   return (
     <>
       <h1>Dashboard</h1>
       <hr />
       <h2>Francisco Marcano</h2>
+      <hr />
+      <h3>Co-Workers</h3>
+      <ul>
+        {coWorkers.map((cw) => (
+          // todo: add eslint key error
+          <li>{cw.name}</li>
+        ))}
+      </ul>
     </>
   );
 };
