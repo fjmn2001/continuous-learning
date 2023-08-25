144e31db081934be36168420bb3ad1654d2a9377
Francisco Marcano <fjmn2001@gmail.com>
2023-08-25 15:27:03 -0400

    adding test

---
+++ apps/show-my-name/tests/sections/dashboard/Dashboard.spec.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+
+import { Dashboard } from "../../../src/sections/dashboard/Dashboard";
+
+describe("Dashboard", () => {
+  it("should render Dashboard component", async () => {
+    render(<Dashboard />);
+
+    const myName = await screen.findByText(/Francisco Marcano/i);
+    expect(myName).toBeInTheDocument();
+  });
+});
