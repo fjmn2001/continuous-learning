65a7a05d2ea4f4d9007e666784a3ea6b5ab17e1c
Francisco Marcano <fjmn2001@gmail.com>
2023-09-15 15:50:30 -0400

    adding testing

--- apps/cash-registers/.eslintrc.js
+++ apps/cash-registers/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: ["custom"],
+  rules: {
+    "import/no-unresolved": "off",
+    "import/no-named-as-default": "off",
+  },
 };
--- apps/cash-registers/tests/sections/sections/auth/LoginForm.spec.tsx
+++ apps/cash-registers/tests/sections/sections/auth/LoginForm.spec.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { LoginForm } from "../../../../src/sections/auth/LoginForm";
 
@@ -6,10 +7,15 @@ describe("LoginForm Test", () => {
   it("should show the component", async () => {
     render(<LoginForm />);
 
-    expect(await screen.findByLabelText(/Correo electrónico/i)).toBeDefined();
-    expect(await screen.findByLabelText(/Contraseña/i)).toBeDefined();
-    expect(
-      await screen.findByRole("button", { name: /Iniciar sesión/i }),
-    ).toBeDefined();
+    const email = await screen.findByLabelText(/Correo electrónico/i);
+    await userEvent.type(email, "email@email.com");
+
+    const password = await screen.findByLabelText(/Contraseña/i);
+    await userEvent.type(password, "123456");
+
+    const submitButton = await screen.findByRole("button", {
+      name: /Iniciar sesión/i,
+    });
+    await userEvent.click(submitButton);
   });
 });
