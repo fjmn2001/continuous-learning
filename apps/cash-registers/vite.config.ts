3a1a7db34acce806171880bfb054f8fc4630ebc1
Francisco Marcano <fjmn2001@gmail.com>
2023-09-29 16:16:14 -0400

    adding cypress test v2

--- apps/cash-registers/vite.config.ts
+++ apps/cash-registers/vite.config.ts
@@ -2,8 +2,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
 
-console.log(path.resolve(__dirname, "./src"), "ddd");
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
